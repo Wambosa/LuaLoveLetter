a09107e5972475790c4257b80544832d2e89ed7b
Osa <shondiaz@gmail.com>
2016-03-16 22:21:20 -0500

    hand view alpha and some responsive touch interactions

--- corona/scene/sandbox.lua
+++ corona/scene/sandbox.lua
@@ -11,11 +11,78 @@ local scene = composer.newScene()
 
 local selCard
 
+--todo: make this a part of the cardView so that i don't have to keep a selCard global
 local function unTiltCard()
     if(selCard.isFocus) then
-        selCard.path.x1, selCard.path.x2, selCard.path.x3 ,selCard.path.x4 = selCard.path.x1*.9, selCard.path.x2*.9, selCard.path.x3*.9 ,selCard.path.x4*.9
-        selCard.path.y1, selCard.path.y2, selCard.path.y3 ,selCard.path.y4 = selCard.path.y1*.9, selCard.path.y2*.9, selCard.path.y3*.9 ,selCard.path.y4*.9
+		local unTilt = .9
+		--todo: try transition for a more clear meaning
+        selCard.path.x1, selCard.path.x2, selCard.path.x3 ,selCard.path.x4 = selCard.path.x1*unTilt, selCard.path.x2*unTilt, selCard.path.x3*unTilt ,selCard.path.x4*unTilt
+        selCard.path.y1, selCard.path.y2, selCard.path.y3 ,selCard.path.y4 = selCard.path.y1*unTilt, selCard.path.y2*unTilt, selCard.path.y3*unTilt ,selCard.path.y4*unTilt
     end
+end
+
+local function onCardHover( event )
+	local card = event.target
+	
+	local phase = event.phase
+	
+	print(event.phase.." detected on "..card.data.name)
+	
+	
+	local deselectCard = function(obj)
+		display.getCurrentStage():setFocus( nil )
+		obj.isFocus = false
+		--transition back down to origin position
+		card.fill.effect = nil
+		transition.cancel(obj)
+		transition.to(obj, {time=100, y=0, width=71, height=100})
+	end
+	
+	if "moved" == phase or "began" == phase then
+	
+		-- Make target the top-most object
+		local parent = card.parent
+		parent:insert( card )
+		
+		display.getCurrentStage():setFocus( card )
+		
+		if(not card.isFocus) then
+			card.y = -100
+			card.width, card.height = 142, 200
+			
+			card.strokeWidth = 5
+			card:setStrokeColor(0,1,0,1)
+			--card.stroke.effect = "generator.marchingAnts"
+			
+			transition.to(card, {time=3000, y=-120, width=142, height=200})
+			--card.fill.effect = "filter.swirl"
+			--transition.to(card.fill.effect, {time=2500, intensity=10})
+			--highlight somehow
+		end
+		
+		-- To prevent this, we add this flag. Only when it's true will "move"
+		card.isFocus = true
+		
+		local trueX, trueY = card:localToContent(0,0)
+		local relX, relY = event.x-trueX, event.y-trueY
+		
+		print("relative", relX, relY)
+		
+		--try to detect upward movement
+		if(relY < -50) then
+			--transition out of handMode and into free mode
+			print("todo: begin free mode for "..card.data.name)
+		end
+		
+		if(math.abs(relX) > 75) then
+			deselectCard(card)
+		end
+            
+	elseif "ended" == phase or "cancelled" == phase then
+		deselectCard(card)
+	end
+	
+	return true
 end
 
 local function onTouch( event )
@@ -25,8 +92,8 @@ local function onTouch( event )
 	local phase = event.phase
 	if "began" == phase then
 		-- Make target the top-most object
-		local parent = card.parent
-		parent:insert( card )
+		--local parent = card.parent
+		--parent:insert( card )
 		display.getCurrentStage():setFocus( card )
 		
         selCard = card
@@ -52,12 +119,11 @@ local function onTouch( event )
             local newY = event.y - card.y0
             
             --if there is any drag movement from the touch then tilt the card
+			--todo: only want this behavior in free drag mode (after users has snapped the card out of hand for use)
             if((newX+newY) ~= math.ceil(oldX+oldY)) then
                 
-                print("old:"..oldY, "new"..newY)
-                
-                local lessX = (card.contentWidth * .0025) + math.abs((card.path.x1+card.path.x2+card.path.x3+card.path.x4)*.5)
-                local lessY = (card.contentHeight * .0025) + math.abs((card.path.y1+card.path.y2+card.path.y3+card.path.y4)*.5)
+                local lessX = (card.contentWidth * .0075) + math.abs((card.path.x1+card.path.x2+card.path.x3+card.path.x4)*.5)
+                local lessY = (card.contentHeight * .005) + math.abs((card.path.y1+card.path.y2+card.path.y3+card.path.y4)*.5)
                 
                 local dragLeft = 0
                 local dragRight = 0
@@ -75,15 +141,17 @@ local function onTouch( event )
                 card.path.x1 = dragLeft + (dragUp*yMod)
                 card.path.x2 = dragLeft + (dragDown*-yMod)
                 card.path.y1 = (dragLeft*xMod) + dragUp
-                card.path.y2 = dragLeft*-xMod + dragDown
+                card.path.y2 = (dragLeft*-xMod) + dragDown
                 
+				card.path.x3 = dragRight + (dragDown*yMod)
                 card.path.x4 = dragRight + (dragUp*-yMod)
-                card.path.x3 = dragRight + (dragDown*yMod)
-                card.path.y4 = dragRight*-xMod + dragUp
-                card.path.y3 = dragRight*xMod + dragDown
+				card.path.y3 = (dragRight*xMod) + dragDown
+                card.path.y4 = (dragRight*-xMod) + dragUp
             end
-            
+			
+            transition.cancel(card) -- seems like a good perf boost. there was some serious lag with abusive drag motions
             transition.to( card, { time=35, x=newX, y=newY } )
+			print("move", "x:"..newX, "y:"..newY)
             
 		elseif "ended" == phase or "cancelled" == phase then
 			display.getCurrentStage():setFocus( nil )
@@ -113,7 +181,7 @@ function scene:create( event )
     local gameOptions = {
         playerCount = 2,
         playerNames = {'James', 'Bill'},
-        startingHandAmount = 1,
+        startingHandAmount = 5, --todo: this is really one. change back to 1 later
         turnDrawAmount = 1
     }
 
@@ -121,25 +189,52 @@ function scene:create( event )
 
     print_r('LOG: game has beeen initialized successfully')
 
+	local cardPixelWidth, cardPixelHeight = 71, 100 --todo: need to make this a percentage (create measured unit or read docs later)
+
+	local deckX, deckY = display.contentCenterX+100, display.contentCenterY
+	
+	local deckImages = display.newGroup()
+	
+	--cascade deck at 45 deg. place 1-2 pixels apart going downward. put them in the deckImages
+	for i=0, #game.deck.playPile, 1 do
+		local cardView
+		cardView = display.newImage(deckImages, 'img/cards/000_back.png')
+		cardView.width, cardView.height = cardPixelWidth*.5, cardPixelHeight*.5
+		cardView.rotation = -45 + math.random(-20, 20) --can make the deck look messy shuffled with a variance to the rotation
+		cardView.x, cardView.y = deckX, deckY-i
+	end
+
+	--todo: create an array of player cardGroups. same as player number to access the group
+	local handImages = display.newGroup()
+	local foeImages = display.newGroup()
+	
+	handImages.x, handImages.y = display.contentCenterX, display.actualContentHeight
+	foeImages.x = display.contentCenterX
+
     for _, player in ipairs(game.players) do
         
-        for __, card in ipairs(player.hand.cards) do
+		local cardsInHand = #player.hand.cards
+		
+        for position, card in ipairs(player.hand.cards) do
             --todo: set player color and touch restriction(only make touchable cards for yourself)
 
 			local cardView
-
+			--todo: need a card/deckView object that is responsible for knowing where it belongs as well as size and various animations/effects (8 is screen limit right now)
 			if(player.name == 'James') then
-				
-				cardView = display.newImage(card.img)
-                cardView.x = display.contentCenterX
-                cardView.y = display.contentCenterY
-				cardView:addEventListener('touch', onTouch)
+				print("starting hand: "..card.name)
+				cardView = display.newImageRect(handImages, card.img, cardPixelWidth, cardPixelHeight)
+				cardView.data = card
+				cardView.x = ((cardsInHand*cardPixelWidth)*-.5) + (cardPixelWidth*position) - (cardPixelWidth*.5)--this last bit is to offset to the center of the card
+				cardView.rotation = (-4.5*(cardsInHand*.5)) + (position*4.5)
+				cardView:addEventListener('touch', onCardHover)
 			else
-				cardView = display.newImage('img/cards/000_back.png')
-                cardView.x = display.contentCenterX
+				print("foe hand: "..card.name)
+				cardView = display.newImage(foeImages, 'img/cards/000_back.png')
+                cardView.x = ((cardsInHand*cardPixelWidth)*-.5) + (cardPixelWidth*position) - (cardPixelWidth*.5)
+				cardView.rotation = 180 + (4.5*(cardsInHand*.5)) - (position*4.5)
 			end
 			
-			cardView.width, cardView.height = 71, 100
+			cardView.width, cardView.height = cardPixelWidth, cardPixelHeight
         end
     end
 end
